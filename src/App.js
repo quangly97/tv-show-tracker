e7a77556a265d145446fd583e20af471a1fc0a0b
Quang Ly <quangly789@hotmail.com>
2021-04-13 22:57:54 -0400

    Added form to allow user input and query with the api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React from 'react'
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 import axios from 'axios'
 
-const title = 'game';
-const URL = `http://api.tvmaze.com/search/shows?q=${title}`;
-
-const fetchShow = () => {
-  axios.get(URL)
-  .then(res => {
-    console.log(res.data);
-  })
-}
-
 function App() {
 
-  fetchShow();
+  const [title, setTitle] = useState('');
+  const URL = `http://api.tvmaze.com/search/shows?q=${title}`;
+
+  const fetchShow = (e) => {
+    e.preventDefault();
+
+    console.log(title);
+
+    axios.get(URL)
+    .then(res => {
+      console.log(res.data);
+    })
+  }
+
+  const changeTitle = (e) => {
+    setTitle(e.target.value);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={fetchShow}>
+          <label htmlFor="search">Show:</label>
+          <input 
+            type="text" 
+            id="search" 
+            name="search"
+            onChange={changeTitle}
+          />
+          <button id='btn'>Submit</button>
+        </form>
       </header>
     </div>
   );
