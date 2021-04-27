243e9d577d05f8e00eb667ef029d845c12f2d99a
Quang Ly <quangly789@hotmail.com>
2021-04-26 22:37:22 -0400

    improved toggle function

--- src/pages/ProgramInformation/ProgramInformation.jsx
+++ src/pages/ProgramInformation/ProgramInformation.jsx
@@ -5,31 +5,43 @@ import { useGlobalContext } from '../../context'
 
 const ProgramInformation = () => {
     const { id } = useParams();
-    const { state } = useGlobalContext();
-    const currentProgram = state.watchlist.find((program) => program.id === parseInt(id));
+    const { state: {watchlist} } = useGlobalContext();
+    const currentProgram = watchlist.find((program) => program.id === parseInt(id));
+    var currentEpisode = currentProgram.episodes.find((episode) => episode.watched === false);
     const [trigger, setTrigger] = useState(false);
 
     useEffect(() => {
       setTrigger(false);
+      currentEpisode = currentProgram.episodes.find((episode) => episode.watched === false);
     }, [trigger])
 
-    const handleClick = (episode) => {
+    const toggleWatched = (episode, index) => {
       episode.watched = !episode.watched;
+      if(currentEpisode && currentEpisode.id !== episode.id && episode.watched === true){
+        togglePrevious(index);
+      }
       setTrigger(true);
+    }
+
+    const togglePrevious = (index) => {
+      const episodelist = currentProgram.episodes;
+      for(var i = 0; i < index; i++){
+        episodelist[i].watched = true;
+      }
     }
 
     return (
         <div>
             <h3>{currentProgram.name}</h3>
             {
-                currentProgram.episodes.map((episode) => {
+                currentProgram.episodes.map((episode, index) => {
                     const { id, name, season, number, airdate, watched } = episode;
                     return (
                       <section key={id}>
                         <span>
                           <h4>{`${name} - S${season} | E${number}`}</h4>
                           <h5>{`${getMonth(airdate.substring(5,7))} ${airdate.substring(8,10)} | ${airdate.substring(0,4)}`}</h5>
-                          <button className="btn" onClick={() => handleClick(episode)}>{`${watched}`}</button>
+                          <button className="btn" onClick={() => toggleWatched(episode, index)}>{`${watched}`}</button>
                         </span>
                       </section>
                     );
