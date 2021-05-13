58a72d1809b55984dd8015b2ca9dcc3652ccec2e
Quang Ly <quangly789@hotmail.com>
2021-05-13 00:27:09 -0400

    Reconfigured Search components and removed Watchlist component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as SearchBar } from './SearchBar/SearchBar';
 export { default as NavBar } from './NavBar/NavBar';
-export { default as WatchList } from './WatchList/WatchList';
 export { default as ProgramCard } from './ProgramCard/ProgramCard';
 export { default as SearchResults } from './SearchResults/SearchResults';
 export { default as Modal } from './Modal/Modal';
--- src/pages/TVShows/TVShows.jsx
+++ src/pages/TVShows/TVShows.jsx
@@ -1,23 +1,74 @@
-import React from 'react';
-import { SearchBar, SearchResults, WatchList, Modal } from '../../components/';
-import { useGlobalContext } from '../../context';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from "../../context";
+import { ProgramCard } from '../../components';
 
 const TVShows = () => {
 
-    const { state: { isModalOpen, modalContent }, dispatch } = useGlobalContext();
+    const { state: { endedWatchlist, runningWatchlist }} = useGlobalContext();
+    const [showEnded, setShowEnded] = useState(true);
+    const [showTBA, setShowTBA] = useState(true);
+    const [showUpcoming, setShowUpcoming] = useState(true);
 
-    const closeModal = () => {
-        dispatch({ type: 'CLOSE_MODAL' });
+    const findLatestEpisode = (program) => {
+        const today = new Date();
+        for(var i = program.episodes.length - 1; i >= 0; i--){
+            const episode = program.episodes[i];
+            const airdate = Date.parse(episode.airstamp);
+
+            if(today - airdate >= 0){
+                return episode;
+            }
+        }
     }
+
+    const findNextEpisode = (program) => {
+        const today = new Date();
+        return program.episodes.find((episode) => today - Date.parse(episode.airstamp) < 0);
+    }
+
+    const upcomingWatchlist = runningWatchlist.filter((program) => findLatestEpisode(program).id !== program.episodes[program.episodes.length - 1].id).map((program) => {
+        program.status = 'Upcoming';
+        return program;
+    });
+
+
+    const tbaWatchlist = runningWatchlist.filter((program) => findLatestEpisode(program).id === program.episodes[program.episodes.length - 1].id).map((program) => {
+        program.status = 'To Be Announced';
+        return program;
+    });
 
     return (
         <div>
+            <Link to='/search'><button className='btn'>+</button></Link>
+            
+            <h2 onClick={() => setShowUpcoming(!showUpcoming)}>UPCOMING</h2>
             {
-                isModalOpen && <Modal closeModal={closeModal} modalContent={modalContent} />
+                showUpcoming && (upcomingWatchlist.map((program) => {
+                    const { id } = program;
+                    return (
+                        <ProgramCard key={id} {...program} nextEpisode={findNextEpisode(program)}/>
+                    )
+                }))
             }
-            <SearchBar/>
-            <SearchResults/>
-            <WatchList/>
+            <h2 onClick={() => setShowTBA(!showTBA)}>TO BE ANNOUNCED</h2>
+            {
+                showTBA && (tbaWatchlist.map((program) => {
+                    const { id } = program
+                    return (
+                        <ProgramCard key={id} {...program}/>
+                    )
+                }))
+            }
+            <h2 onClick={() => setShowEnded(!showEnded)}>ENDED</h2>
+            {
+                showEnded && (endedWatchlist.map((program) => {
+                    const { id } = program
+                    return (
+                        <ProgramCard key={id} {...program}/>
+                    )
+                }))
+            }
         </div>
     )
 }
